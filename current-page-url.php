0f54948c9fdac1268d1e5149c72d1e8f7ae40b29
Alireza Sedghi <alirezasedghi911@gmail.com>
2018-07-16 22:37:43 +0430

    add "Get the Current Page URL"

---
+++ current-page-url.php
@@ -0,0 +1,27 @@
+<?php
+
+// Get the Current Page URL
+
+function page_URL() {
+	
+	// Set Protocol
+    if ( isset( $_SERVER["HTTPS"] ) && !empty( $_SERVER["HTTPS"] ) && ( $_SERVER["HTTPS"] != 'on' ) ) {
+        $url = 'https://' . $_SERVER["SERVER_NAME"];
+    } else {
+        $url = 'http://' . $_SERVER["SERVER_NAME"];
+    }
+	
+	// Add Port
+    if ( ( $_SERVER["SERVER_PORT"] != 80 ) ) {
+        $url .= $_SERVER["SERVER_PORT"];
+    }
+	
+	// Build URL
+    $url .= $_SERVER["REQUEST_URI"];
+    return $url;
+	
+}
+ 
+echo page_URL();
+
+?>
